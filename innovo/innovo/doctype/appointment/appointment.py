d5135f8ad78f1ca856ca096bdc330d30d1c509a6
Your Name <your.email@example.com>
2025-10-07 11:35:27 +0200

    feat: cleaning up appointment codes

--- innovo/innovo/doctype/appointment/appointment.py
+++ innovo/innovo/doctype/appointment/appointment.py
@@ -1,28 +1,6 @@
 # Copyright (c) 2025, Arnold Simony and contributors
 # For license information, please see license.txt
 
-<<<<<<< HEAD
-
-import frappe
-import datetime
-from frappe.utils import get_datetime
-
-def validate(doc, method=None):
-    """Ensure duration is correct when saving via API/import/etc."""
-    if doc.start_datetime and doc.end_datetime:
-        start = get_datetime(doc.start_datetime)
-        end = get_datetime(doc.end_datetime)
-        if end < start:
-            frappe.throw("End Time must be after Start Time.")
-        minutes = int((end - start).total_seconds() // 60)
-        doc.duration = minutes
-# # import frappe
-# from frappe.model.document import Document
-
-
-# class Appointment(Document):
-# 	pass
-=======
 import frappe
 from frappe.model.document import Document
 from frappe.utils import get_datetime, time_diff_in_seconds
@@ -85,4 +63,4 @@ class Appointment(Document):
 				f"Please choose a different time slot or location.",
 				title="Double Booking Detected"
 			)
->>>>>>> dadb71e66318f649833b741e4ec75373b94f7da1
+
