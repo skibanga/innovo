1b947f229536326b6ffd7d978c7a53dc248cdafe
Your Name <your.email@example.com>
2025-12-01 13:50:33 +0100

    feat: changes as per comments from justine.

---
+++ innovo/innovo/doctype/bank_disbursement/bank_disbursement.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Arnold Simony and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Bank Disbursement", {
+// 	refresh(frm) {
+
+// 	},
+// });
